f3c20aa5886aaffbb9d79f0cda8350124e8d3259
programint <git config --global user.email>
2025-03-10 14:08:02 +0800

    不同价格的销售税

---
+++ 1_Fixed_Size_Data/ex58.rkt
@@ -0,0 +1,19 @@
+; 58
+
+; 计算不同价格的销售税 
+; Price -> Number
+
+(define LOW 0.05)
+(define LUX 0.08)
+
+(define (sales-tax p)
+  (cond
+    [(and (<= 0 p) (< p 1000)) 0]
+    [(and (<= 1000 p) (< p 10000)) (* LOW p)]
+    [(>= p 10000) (* LUX p)]))
+
+(check-expect (sales-tax 537) 0)
+(check-expect (sales-tax 1000) (* LOW 1000))
+(check-expect (sales-tax 1282) (* LOW 1282))
+(check-expect (sales-tax 10000) (* LUX 10000))
+(check-expect (sales-tax 12017) (* LUX 12017))
