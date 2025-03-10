17700fb3dd38709faa7dad29d4a096afb8e543bd
programint <git config --global user.email>
2025-03-10 14:37:05 +0800

    理解结构体数据定义

---
+++ 1_Fixed_Size_Data/ex77.rkt
@@ -0,0 +1,20 @@
+; 77
+
+; 结构体定义，回看 5.4 节：定义结构体类型 (Defining Structure Types)
+
+(define-struct time[hour minute second])
+; A time is a structure
+    ; (make-time Number Number Number)
+    ; 解释:某一时间点，由小时，分钟，秒构成，24 小时制方式表示这一具体时间
+
+; where:
+; - hour is a Number in [0, 23]
+; - minute is a Number in [0, 59]
+; - second is a Number in [0, 59]
+
+; 示例 (Examples):
+(define example-time (make-time 14 30 15)) ; 下午2点30分15秒
+(define another-time (make-time 0 0 0))    ; 表示午夜0点整
+(define moring-time (make-time 09 0 0))    ; 表示上午9点整
+
+
