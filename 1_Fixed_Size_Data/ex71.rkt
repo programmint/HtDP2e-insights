76b3001bae62f9b72c13c876a1d47bd328e4f628
programint <git config --global user.email>
2025-03-10 14:33:25 +0800

    求值

---
+++ 1_Fixed_Size_Data/ex71.rkt
@@ -0,0 +1,24 @@
+; 71
+
+; distances in terms of pixels:
+
+(define HEIGHT 200)
+(define MIDDLE (quotient HEIGHT 2))
+(define WIDTH  400)
+(define CENTER (quotient WIDTH 2))
+
+(define-struct game [left-player right-player ball])
+
+(define game0
+    (make-game MIDDLE MIDDLE (make-posn CENTER CENTER)))
+
+(game-ball game0) 
+; (make-posn 200 200)
+
+(posn? (game-ball game0)) 
+; #true
+
+(game-left-player game0)
+; 100
+
+
