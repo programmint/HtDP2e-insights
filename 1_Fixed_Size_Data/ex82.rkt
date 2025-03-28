d73f095b4a544468a5b9184e4704ed10fc497b01
programint <git config --global user.email>
2025-03-28 21:29:32 +0800

    设计结构体

--- 1_Fixed_Size_Data/ex82.rkt
+++ 1_Fixed_Size_Data/ex82.rkt
@@ -1,63 +1,60 @@
 ; 82
 
-; 解题
-; 犯了一个错误，我以为输出的是字符串，其实输出的应该是结构体
-; 题目中讲的是单词，这个单词的数据格式是什么？
+; 思考点
+; 本题承接自 78 题，word 是结构体。
+; 所以，82 题输入是结构体，输出必然是结构体。
 
-; 第1、 这单词，其实由字符串和布尔值构成，所以，要么是条目，要么是结构体
-; 第2、该题需要依据**字段名**来比较数据是否相同，既要根据位置，取出对应的比较对象，又要比较对应的语义，所以只能是结构体。
+; 之前犯了一错误，以为输出的是字符串。需求没有搞清楚，急于写代码。
+; 之前的第 1 版代码写复杂了，现在优化为第 2 版。
 
-; 即：该题的输入和输出，都是结构体。
+; 第 2 版代码 -----------------------------------------------------------------------------------------
 
-; -----------------------------------------------------------------------------------------------------------------------------------------------
+; 全局目的
+; 比较两个单词（结构体），每个字母是否相同
+; 单词由3个小写字母组成，字母"a"到"z"的 1 string 外加 #false 表示
+; -比较规则
+;  -- 相同位置字母相同 → 保留该字母
+;  -- 不同 → 该字母替换为 #false
 
-; 定义输入结构体：给主函数输入数据
-; 这里不必校验输入是否准确，因为第 75 题已经明确了是字符串
-(define-struct input-word [1st-letter 2nd-letter 3rd-letter])
 
-; word是结构体，由3个字母构成，小写字母（ ”a“ 至 ”z“ ）构成，每个字符串长度为1
-; (make-input-word string string string)
-; 示例
-; (define a-word (make-input-word "a" "r" "t"))
+; word 是结构体
+(define-struct word [1st 2nd 3rd])
+;   (make-word string string string )
+; 解释:由3个字母构成，小写字母（ ”a“ 至 ”z“ ）构成，每个字符串长度为1
+; ex：(make-word "c" "a" "t")
 
-; 定义输出结构体：主函数以该结构体格式，输出数据
-(define-struct output-word [1st-letter 2nd-letter 3rd-letter])
-
-; output-word 是结构体，由3个字母或 #false 构成，字母是小写字母（ ”a“ 至 ”z“ ）构成，每个字符串长度为1
-; (make-output-word string string string)
-; 示例
-; (define a-word(make-output-word "a" "d" "d"))
-
-; 定义字母比较函数：辅助函数，主函数用以比较2个字母是否相同
+; 辅函数
+; 为主函数比较字母是否相同
 ; 相同，返回该字母
-; 不相同，则返回 #false
-; string string -> string or boolean
-(define (compare-letters 1st-letter 2nd-letter)
+; 不相同，返回 #false
+; 1string or #false -> 1string or #false
+(define (compare-letter letter1 letter2)
     (cond
-        [(equal? 1st-letter 2nd-letter) 1st-letter]
+        [(equal? letter1 letter2) letter1]
         [else #false]))
+    
+    ; 测试辅函数
+    (check-expect (compare-letter "a" "b") #false)
+    (check-expect (compare-letter "a" "a") "a")
 
-; 测试字母比较函数
-(check-expect (compare-letters "a" "b") #false)
-(check-expect (compare-letters "a" "a") "a")
+; 主函数
+; 生成比较结果的结构体
+; word word -> word 
+(define (compare-word word1 word2)
+    (make-word
+        (compare-letter (word-1st word1)(word-1st word2))
+        (compare-letter (word-2nd word1)(word-2nd word2))
+        (compare-letter (word-3rd word1)(word-3rd word2))))
 
-; 定义主函数 compare-word
-; 读取 2 个输入结构体，比较，返回结构体
-; 每一字段相同，则返回该结构体字段
-; 不相同，返回的结构体字段中放入 #false 
-; structure structure -> structure
-(define (compare-word input-word1 input-word2 )
-    (make-output-word
-       (compare-letters (input-word-1st-letter input-word1) (input-word-1st-letter input-word2))
-       (compare-letters (input-word-2nd-letter input-word1) (input-word-2nd-letter input-word2))
-       (compare-letters (input-word-3rd-letter input-word1) (input-word-3rd-letter input-word2))))
+    ; 测试主函数
+    (check-expect
+        (compare-word (make-word "o" "n" "e") (make-word "t" "w" "o"))
+        (make-word #false #false #false))
 
-; 测试主函数
-(check-expect (compare-word (make-input-word "a" "r" "t")(make-input-word "a" "r" "t")) (make-output-word "a" "r" "t"))
-(check-expect (compare-word (make-input-word "a" "d" "d")(make-input-word "a" "r" "t")) (make-output-word "a" #false #false))
+    (check-expect
+        (compare-word (make-word "a" "r" "t") (make-word "a" "r" "t"))
+        (make-word "a" "r" "t"))
 
-(compare-word (make-input-word "a" "r" "t") (make-input-word "c" "a" "n"))
-; 输出：(make-output-word #false #false #false)
-
-(compare-word (make-input-word "a" "r" "t") (make-input-word "a" "r" "t"))
-; 输出：(make-output-word  "a" "r" "t")
+    (check-expect
+        (compare-word (make-word #false #false #false) (make-word #false #false #false))
+        (make-word #false #false #false))
