c8b41ba302cddd39949c6478fc5fd1346c89dcc2
programint <git config --global user.email>
2025-03-30 07:24:38 +0800

    优化缩进及空格

--- 1_Fixed_Size_Data/ex20.rkt
+++ 1_Fixed_Size_Data/ex20.rkt
@@ -1,8 +1,8 @@
 ; 20
 
 (define (string-delete str i)
-    (string-append(substring str 0 i)(substring str (+ i 1))))
+  (string-append (substring str 0 i) (substring str (+ i 1))))
 
 (string-delete "htdp2e" 3)
 
-; 16 ~ 20 题，都是加深你对函数的理解
+; 16 ~ 20 题,都是加深你对函数的理解
