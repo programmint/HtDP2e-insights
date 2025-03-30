099e56c2384dda8b3716637cde047288a232f8f7
programint <git config --global user.email>
2025-03-30 18:53:10 +0800

    优化缩进及连接符

--- 1_Fixed_Size_Data/ex18.rkt
+++ 1_Fixed_Size_Data/ex18.rkt
@@ -1,10 +1,8 @@
 ; 18
 
 (define (string-join str1 str2)
-    (string-append str1 "_" str2))
+  (string-append str1 "-" str2))
 
 (string-join "Htdp" "2e") 
 
-; 考察定义函数，以及 string-append 用法
-
-
+; 考察定义函数,以及 string-append 用法
--- 1_Fixed_Size_Data/ex19.rkt
+++ 1_Fixed_Size_Data/ex19.rkt
@@ -3,7 +3,7 @@
 (define (string-insert str i)
   (string-append
    (substring str 0 i)
-   "_"
+   "-"
    (substring str i)))
 
 (string-insert "LearningHtdp2e" 8)
